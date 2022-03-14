31d9a8215431ecaa7b062712c563908602052662
MarinaRomanovna <kokanovamarina95>
2022-03-14 20:44:19 +0500

    fixes SP, extension

--- app/src/main/java/ru/wobcorp/justforpractice/utils/ViewExtensions.kt
+++ app/src/main/java/ru/wobcorp/justforpractice/utils/ViewExtensions.kt
@@ -4,8 +4,8 @@ import android.graphics.Color
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 
-fun View.showSnackbar(text: String) {
-    Snackbar.make(this, text, Snackbar.LENGTH_LONG)
+fun showSnackbar(view: View, text: String) {
+    Snackbar.make(view, text, Snackbar.LENGTH_LONG)
         .setBackgroundTint(Color.DKGRAY)
         .show()
 }
